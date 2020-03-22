86a6ccf05914432c35c60691e2f7e507181be169
Vitor casa <vitor.r.soares@gmail.com>
2020-03-22 02:41:36 -0300

    Add jobs aula 17 obs:não funcionou a fila

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "39f70ac4f4467b",
+    pass: "8287b36ea7bd76",
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
